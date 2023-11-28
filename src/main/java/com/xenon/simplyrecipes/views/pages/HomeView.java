7322c7d331968409f5890fd904e0a56babe675ce
Alex Xenon <b3haply@gmail.com>
2023-11-28 11:03:44 +0200

    Styling recipe-card

--- src/main/java/com/xenon/simplyrecipes/views/pages/HomeView.java
+++ src/main/java/com/xenon/simplyrecipes/views/pages/HomeView.java
@@ -7,9 +7,11 @@ import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.xenon.simplyrecipes.entities.Recipe;
+import com.xenon.simplyrecipes.services.RecipeService;
 import com.xenon.simplyrecipes.views.MainLayout;
 import com.xenon.simplyrecipes.views.components.AddRecipeDialog;
 import com.xenon.simplyrecipes.views.components.RecipeCardLayout;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 
@@ -18,14 +20,28 @@ import java.time.LocalDate;
 public class HomeView extends Main {
 
     AddRecipeDialog addRecipeDialog = new AddRecipeDialog();
+    @Autowired
+    private RecipeService recipeService;
 
-    public HomeView() {
+    public HomeView(RecipeService recipeService) {
+        this.recipeService = recipeService;
+
         add(new H2("Welcome to Simply Recipes!"));
         addClassName("container");
 
         Button addRecipe = new Button("Add Recipe", e -> addRecipeDialog.open());
         add(addRecipe);
 
+        Div pageContent = new Div();
+        pageContent.addClassName("page-content");
+
+        recipeService.getAllRecipes().forEach(recipe -> pageContent.add(new RecipeCardLayout(recipe)));
+
+        add(pageContent);
+    }
+
+
+    private void initialize(Div pageContent) {
         Recipe recipe = new Recipe();
         recipe.setName("Pizza");
         recipe.setDescription("Pepperoni is a variety of spicy salami made from cured pork and beef seasoned with paprika or other chili pepper. Prior to cooking, pepperoni is characteristically soft, slightly smoky, and bright red.");
@@ -38,9 +54,6 @@ public class HomeView extends Main {
         recipe.setCookingSteps(null);
         recipe.setDateCreated(LocalDate.now());
 
-        Div pageContent = new Div();
-        pageContent.addClassName("page-content");
-
         pageContent.add(new RecipeCardLayout(recipe));
         pageContent.add(new RecipeCardLayout(recipe));
         pageContent.add(new RecipeCardLayout(recipe));
@@ -50,7 +63,5 @@ public class HomeView extends Main {
         pageContent.add(new RecipeCardLayout(recipe));
         pageContent.add(new RecipeCardLayout(recipe));
         pageContent.add(new RecipeCardLayout(recipe));
-
-        add(pageContent);
     }
 }
